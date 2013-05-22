0088796ca7fff30bd02017c5efd95c8e39a8520e
Eric D Helms <ehelms@redhat.com>
2013-05-22 12:29:09 -0400

    Updating component scaffold to account for new release and build tasks. Also includes the new structure created first in the Alchemy component.

--- test/directives/example_test.js
+++ test/directives/example_test.js
@@ -4,12 +4,12 @@ describe('Directive: Example', function () {
   var element;
   
   beforeEach(module('example'));
-
-  beforeEach(module('component/templates/example_directive_template.html'));
+  beforeEach(module('alchemy', 'alch-templates'));
 
   it('should make hidden element visible', inject(function ($rootScope, $compile) {
     element = angular.element('<blah></blah>');
     element = $compile(element)($rootScope);
     expect(element.text()).toBe('');
   }));
+
 });
